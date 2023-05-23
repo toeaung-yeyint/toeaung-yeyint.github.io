b5a0147e367ed1337e4c0ccd72bccfc8afd17d31
Toe Aung Yint <ToeAung.Yint@monash.edu>
2023-05-23 16:23:16 +1000

    added intro text section and my project section

--- src/index.js
+++ src/index.js
@@ -1,47 +1,62 @@
 import { html, css, LitElement } from "./lit-core.min.js";
 
 class customElement extends LitElement {
-	static properties = {
-		inputs: { type: Object },
-	};
+  static properties = {
+    inputs: { type: Object },
+  };
 
-	static styles = css``;
+  static styles = css``;
 
-	constructor() {
-		super();
-	}
+  constructor() {
+    super();
+  }
 
-	submitForm(e) {
-		e.preventDefault();
-		console.log(this.renderRoot.querySelector("#username").value);
-		console.log(typeof this.renderRoot.querySelector("#username").value);
-	}
+  submitForm(e) {
+    e.preventDefault();
+    console.log(this.renderRoot.querySelector("#username").value);
+    console.log(typeof this.renderRoot.querySelector("#username").value);
+  }
 
-	checkUser() {
-		if (this.inputs.name) {
-			return `Welcome to the app: ${this.inputs.name.toUpperCase()}`;
-		} else {
-			return `Please login in.`;
-		}
-	}
+  checkUser() {
+    if (this.inputs.name) {
+      return `Welcome to the app: ${this.inputs.name.toUpperCase()}`;
+    } else {
+      return `Please login in.`;
+    }
+  }
 
-	listSkills() {
-		const skills = this.inputs.skills.filter((skill) => {
-			if (skill !== "Typescript" && skill !== "React") {
-				return true;
-			}
-		});
-		return skills.map((skill) => html`<li>${skill}</li>`);
-	}
+  listSkills() {
+    const skills = this.inputs.skills.filter((skill) => {
+      if (skill !== "Typescript" && skill !== "React") {
+        return true;
+      }
+    });
+    return skills.map((skill) => html`<li>${skill}</li>`);
+  }
 
-	render() {
-		console.log(this.inputs);
-		console.log(this.renderRoot.querySelector("p"));
-		return html` <p>${this.checkUser()}</p>
-			<ul class="list">
-				${this.listSkills()}
-			</ul>`;
-	}
+  render() {
+    console.log(this.inputs);
+    console.log(this.renderRoot.querySelector("p"));
+    return html` <p>${this.checkUser()}</p>
+      <ul class="list">
+        ${this.listSkills()}
+      </ul>`;
+  }
 }
 
 customElements.define("custom-element", customElement);
+
+const splitLetters = () => {
+  const element = document.querySelector(".epicText");
+  const splitedLetters = document
+    .querySelector(".epicText")
+    .textContent.split("");
+  element.innerText = "";
+  splitedLetters.forEach((letter) => {
+    const span = document.createElement("span");
+    span.classList.add("letter");
+    span.innerText = letter;
+    element.append(span);
+  });
+};
+splitLetters();
