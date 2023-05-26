3c46c0b297c15a619ffe6457bfedb920f7f4f468
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2023-05-26 23:28:33 +1000

    added my project section

--- dist/index.3bd1722b.js
+++ dist/index.3bd1722b.js
@@ -20,140 +20,140 @@ const t=window,e=t.ShadowRoot&&(void 0===t.ShadyCSS||t.ShadyCSS.nativeShadow)&&"
  * SPDX-License-Identifier: BSD-3-Clause
  */
 var st,it;class rt extends f{constructor(){super(...arguments),this.renderOptions={host:this},this.st=void 0}createRenderRoot(){var t,e;const s=super.createRenderRoot();return null!==(t=(e=this.renderOptions).renderBefore)&&void 0!==t||(e.renderBefore=s.firstChild),s}update(t){const e=this.render();this.hasUpdated||(this.renderOptions.isConnected=this.isConnected),super.update(t),this.st=((t,e,s)=>{var i,r;const n=null!==(i=null==s?void 0:s.renderBefore)&&void 0!==i?i:e;let o=n._$litPart$;if(void 0===o){const t=null!==(r=null==s?void 0:s.renderBefore)&&void 0!==r?r:null;n._$litPart$=o=new Z(e.insertBefore(C(),t),t,void 0,null!=s?s:{})}return o._$AI(t),o})(e,this.renderRoot,this.renderOptions)}connectedCallback(){var t;super.connectedCallback(),null===(t=this.st)||void 0===t||t.setConnected(!0)}disconnectedCallback(){var t;super.disconnectedCallback(),null===(t=this.st)||void 0===t||t.setConnected(!1)}render(){return V}}rt.finalized=!0,rt._$litElement$=!0,null===(st=globalThis.litElementHydrateSupport)||void 0===st||st.call(globalThis,{LitElement:rt});const nt=globalThis.litElementPolyfillSupport;null==nt||nt({LitElement:rt});(null!==(it=globalThis.litElementVersions)&&void 0!==it?it:globalThis.litElementVersions=[]).push("3.3.2");class ot extends rt{static properties={inputs:{type:Object}};static styles=n`
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6 {
-      font-family: Roboto Condensed, sans-serif;
-    }
-    a {
-      text-decoration: none;
-    }
-    a .featurebox {
-      box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-      padding: 0;
-      overflow: hidden;
-      height: 100%;
-    }
-    a .featurebox:hover .featureboxTitle::after {
-      opacity: 1;
-    }
-    a .featurebox:hover .featureboxImage {
-      transform: scale(1.1);
-    }
-    a .featurebox .featureboxImageContainer {
-      width: 100%;
-      height: 220px;
-      max-height: 220px;
-      overflow: hidden;
-    }
-    a .featurebox .featureboxImageContainer .featureboxImage {
-      width: 100%;
-      height: 100%;
-      transition: transform 600ms ease;
-      object-fit: cover;
-    }
-    a .featurebox .featureboxTitle {
-      color: #006dae;
-      font-size: 1.2rem;
-      position: relative;
-      margin: 0;
-      padding: 15px;
-      text-align: center;
-      font-weight: bold;
-    }
-    a .featurebox .featureboxTitle::after {
-      content: "";
-      width: 100%;
-      height: 5px;
-      background: linear-gradient(transparent, #007aff, transparent);
-      position: absolute;
-      top: -2.5px;
-      left: 0;
-      opacity: 0;
-      transition: opacity 600ms ease;
-      z-index: 1000;
-    }
-  `;constructor(){super()}render(){return z` <a target="_blank" href="${this.inputs.link}">
-      <div class="featurebox">
-        <div class="featureboxImageContainer">
-          <img
-            class="featureboxImage"
-            src="${this.inputs.src?this.inputs.src:"https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg"}"
-            alt="${this.inputs.alt?this.inputs.alt:"placeholder"}"
-          />
-        </div>
-        <h3 class="featureboxTitle">
-          ${this.inputs.title?this.inputs.title:"placeholder"}
-        </h3>
-      </div>
-    </a>`}}class at extends rt{static properties={name:{type:String}};static styles=n`
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6 {
-      font-family: Roboto Condensed, sans-serif;
-    }
-    .myName {
-      font-size: 40px;
-      text-align: center;
-      margin-bottom: 30px;
-    }
-    .myName .letter {
-      display: inline-block;
-      transition: transform 600ms ease;
-    }
-    .myName:hover .letter:nth-child(1) {
-      transform: translate(-20px, -20px) rotate(-20deg);
-    }
-    .myName:hover .letter:nth-child(2) {
-      transform: translate(-10px, 20px) rotate(10deg);
-    }
-    .myName:hover .letter:nth-child(3) {
-      transform: translate(-10px, -20px) rotate(-10deg);
-    }
-    .myName:hover .letter:nth-child(4) {
-      transform: translate(0px, 10px) rotate(20deg);
-    }
-    .myName:hover .letter:nth-child(5) {
-      transform: translate(-10px, -40px) rotate(-20deg);
-    }
-    .myName:hover .letter:nth-child(6) {
-      transform: translate(-10px, 10px) rotate(10deg);
-    }
-    .myName:hover .letter:nth-child(7) {
-      transform: translate(-10px, -20px) rotate(-10deg);
-    }
-    .myName:hover .letter:nth-child(8) {
-      transform: translate(0, 20px) rotate(10deg);
-    }
-    .myName:hover .letter:nth-child(9) {
-      transform: translate(0, -30px) rotate(-20deg);
-    }
-    .myName:hover .letter:nth-child(10) {
-      transform: translate(0, 10px) rotate(10deg);
-    }
-    .myName:hover .letter:nth-child(11) {
-      transform: translate(10px, -20px) rotate(-10deg);
-    }
-    .myName:hover .letter:nth-child(12) {
-      transform: translate(10px, 10px) rotate(20deg);
-    }
-    .myName:hover .letter:nth-child(13) {
-      transform: translate(20px, -10px) rotate(-10deg);
-    }
-    .myName:hover .letter:nth-child(14) {
-      transform: translate(20px, 20px) rotate(20deg);
-    }
-    .myName:hover .letter:nth-child(15) {
-      transform: translate(20px, -30px) rotate(-10deg);
-    }
-    .myName:hover .letter:nth-child(16) {
-      transform: translate(25px, 10px) rotate(10deg);
-    }
-  `;constructor(){super()}render(){const t=document.createElement("h1");t.innerText=`${this.name}`,t.classList.add("myName"),this.renderRoot.append(t);const e=this.renderRoot.querySelector(".myName"),s=this.renderRoot.querySelector(".myName").textContent.split("");e.innerText="",s.forEach((e=>{const s=document.createElement("span");s.classList.add("letter"),s.style.display="inline-block"," "===e?(s.innerText="-",s.style.visibility="hidden"):s.innerText=e,t.append(s)}))}}customElements.define("feature-box",ot),customElements.define("my-name",at);
-//# sourceMappingURL=index.a704e25f.js.map
+		h1,
+		h2,
+		h3,
+		h4,
+		h5,
+		h6 {
+			font-family: Roboto Condensed, sans-serif;
+		}
+		a {
+			text-decoration: none;
+		}
+		a .featurebox {
+			box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+			padding: 0;
+			overflow: hidden;
+			height: 100%;
+		}
+		a .featurebox:hover .featureboxTitle::after {
+			opacity: 1;
+		}
+		a .featurebox:hover .featureboxImage {
+			transform: scale(1.1);
+		}
+		a .featurebox .featureboxImageContainer {
+			width: 100%;
+			height: 220px;
+			max-height: 220px;
+			overflow: hidden;
+		}
+		a .featurebox .featureboxImageContainer .featureboxImage {
+			width: 100%;
+			height: 100%;
+			transition: transform 600ms ease;
+			object-fit: cover;
+		}
+		a .featurebox .featureboxTitle {
+			color: #006dae;
+			font-size: 1.2rem;
+			position: relative;
+			margin: 0;
+			padding: 15px;
+			text-align: center;
+			font-weight: bold;
+		}
+		a .featurebox .featureboxTitle::after {
+			content: "";
+			width: 100%;
+			height: 5px;
+			background: linear-gradient(transparent, #007aff, transparent);
+			position: absolute;
+			top: -2.5px;
+			left: 0;
+			opacity: 0;
+			transition: opacity 600ms ease;
+			z-index: 1000;
+		}
+	`;constructor(){super()}render(){return z` <a target="_blank" href="${this.inputs.link}">
+			<div class="featurebox">
+				<div class="featureboxImageContainer">
+					<img
+						class="featureboxImage"
+						src="${this.inputs.src?this.inputs.src:"https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg"}"
+						alt="${this.inputs.alt?this.inputs.alt:"placeholder"}"
+					/>
+				</div>
+				<h3 class="featureboxTitle">
+					${this.inputs.title?this.inputs.title:"placeholder"}
+				</h3>
+			</div>
+		</a>`}}class at extends rt{static properties={name:{type:String}};static styles=n`
+		h1,
+		h2,
+		h3,
+		h4,
+		h5,
+		h6 {
+			font-family: Roboto Condensed, sans-serif;
+		}
+		.myName {
+			font-size: 54px;
+			text-align: center;
+			margin-bottom: 30px;
+		}
+		.myName .letter {
+			display: inline-block;
+			transition: transform 600ms ease;
+		}
+		.myName:hover .letter:nth-child(1) {
+			transform: translate(-20px, -20px) rotate(-20deg);
+		}
+		.myName:hover .letter:nth-child(2) {
+			transform: translate(-10px, 20px) rotate(10deg);
+		}
+		.myName:hover .letter:nth-child(3) {
+			transform: translate(-10px, -20px) rotate(-10deg);
+		}
+		.myName:hover .letter:nth-child(4) {
+			transform: translate(0px, 10px) rotate(20deg);
+		}
+		.myName:hover .letter:nth-child(5) {
+			transform: translate(-10px, -40px) rotate(-20deg);
+		}
+		.myName:hover .letter:nth-child(6) {
+			transform: translate(-10px, 10px) rotate(10deg);
+		}
+		.myName:hover .letter:nth-child(7) {
+			transform: translate(-10px, -20px) rotate(-10deg);
+		}
+		.myName:hover .letter:nth-child(8) {
+			transform: translate(0, 20px) rotate(10deg);
+		}
+		.myName:hover .letter:nth-child(9) {
+			transform: translate(0, -30px) rotate(-20deg);
+		}
+		.myName:hover .letter:nth-child(10) {
+			transform: translate(0, 10px) rotate(10deg);
+		}
+		.myName:hover .letter:nth-child(11) {
+			transform: translate(10px, -20px) rotate(-10deg);
+		}
+		.myName:hover .letter:nth-child(12) {
+			transform: translate(10px, 10px) rotate(20deg);
+		}
+		.myName:hover .letter:nth-child(13) {
+			transform: translate(20px, -10px) rotate(-10deg);
+		}
+		.myName:hover .letter:nth-child(14) {
+			transform: translate(20px, 20px) rotate(20deg);
+		}
+		.myName:hover .letter:nth-child(15) {
+			transform: translate(20px, -30px) rotate(-10deg);
+		}
+		.myName:hover .letter:nth-child(16) {
+			transform: translate(25px, 10px) rotate(10deg);
+		}
+	`;constructor(){super()}render(){const t=document.createElement("h1");t.innerText=`${this.name}`,t.classList.add("myName"),this.renderRoot.append(t);const e=this.renderRoot.querySelector(".myName"),s=this.renderRoot.querySelector(".myName").textContent.split("");e.innerText="",s.forEach((e=>{const s=document.createElement("span");s.classList.add("letter"),s.style.display="inline-block"," "===e?(s.innerText="-",s.style.visibility="hidden"):s.innerText=e,t.append(s)}))}}customElements.define("feature-box",ot),customElements.define("my-name",at);
+//# sourceMappingURL=index.3bd1722b.js.map
