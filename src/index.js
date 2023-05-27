48bd7dd985c852ee1f06d3682ca7606e46931ae1
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2023-05-27 21:40:15 +1000

    improved intro section and my project section

--- src/index.js
+++ src/index.js
@@ -99,7 +99,7 @@ class MyName extends LitElement {
 			font-family: Roboto Condensed, sans-serif;
 		}
 		.myName {
-			font-size: 45px;
+			font-size: 40px;
 			text-align: center;
 			margin-bottom: 30px;
 		}
