422c550d74c7bbffed86eb7e736ca22ddff0e7e1
Toe Aung Yint <toeaung.yint@monash.edu>
2023-07-21 11:38:06 +1000

    rebuild my portfolio website using vue 3

--- src/App.vue
+++ src/App.vue
@@ -6,22 +6,25 @@ import ProjectCard from "./components/ProjectCard.vue";
 </script>
 
 <template>
-	<div class="max-w-screen-lg mx-auto">
-		<NavigationBar />
-		<AboutSection />
-		<MyProjectsSection>
-			<ProjectCard
-				projectTitle="teams-titles"
-				projectImage="https://images.unsplash.com/photo-1553778263-73a83bab9b0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2371&q=80"
-			/>
-			<ProjectCard
-				projectTitle="teams-titles"
-				projectImage="https://images.unsplash.com/photo-1553778263-73a83bab9b0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2371&q=80"
-			/>
-			<ProjectCard
-				projectTitle="teams-titles"
-				projectImage="https://images.unsplash.com/photo-1553778263-73a83bab9b0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2371&q=80"
-			/>
-		</MyProjectsSection>
-	</div>
+  <div class="max-w-screen-lg mx-auto">
+    <NavigationBar />
+    <AboutSection />
+    <MyProjectsSection>
+      <ProjectCard
+        projectTitle="teams-titles"
+        projectImage="/assets/teams-titles.jpg"
+        projectLink="https://toeaung-yeyint.github.io/teams-titles/"
+      />
+      <ProjectCard
+        projectTitle="calculate-shapes-volume"
+        projectImage="/assets/calculate-shapes-volume.jpg"
+        projectLink="https://toeaung-yeyint.github.io/calculate-shapes-volume/"
+      />
+      <ProjectCard
+        projectTitle="image-object-detection"
+        projectImage="/assets/image-object-detection.jpg"
+        projectLink="https://toeaung-yeyint.github.io/image-object-detection/"
+      />
+    </MyProjectsSection>
+  </div>
 </template>
--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,26 +1,32 @@
 <template>
-	<div class="flex flex-col gap-x-8 mt-16 px-4">
-		<h1 class="text-center text-3xl font-bold mb-10">Toe Aung Ye Yint</h1>
-		<p class="text-base">
-			I am a skilled front-end developer with a diverse educational background,
-			I hold a Master's degree in Information Technology (Monash University,
-			Australia) alongside a Bachelor's degree in Architecture (Thanlyin
-			Technological University, Myanmar). This unique blend of qualifications
-			provides me with a refined sense of design aesthetics and a deep
-			understanding of technology. By seamlessly integrating creativity with
-			technical expertise, I offer a distinctive perspective that bridges the
-			gap between form and function, delivering exceptional solutions in the
-			field of front-end development.
-		</p>
-		<div class="flex items-center mt-10 mb-16 gap-6 flex-wrap">
-			<p class="text-lg italic">Tech stacks:</p>
-			<div class="flex gap-6">
-				<img class="w-7" src="/assets/html_logo.jpg" alt="" />
-				<img class="w-7" src="/assets/css_logo.jpg" alt="" />
-				<img class="w-7" src="/assets/javascript_logo.jpg" alt="" />
-			</div>
-		</div>
-	</div>
+  <div class="flex flex-col gap-x-8 mt-16 px-4">
+    <h1 class="text-center text-3xl font-bold mb-10">Toe Aung Ye Yint</h1>
+    <p class="text-base">
+      I am a skilled front-end developer with a diverse educational background,
+      I hold a Master's degree in Information Technology (Monash University,
+      Australia) alongside a Bachelor's degree in Architecture (Thanlyin
+      Technological University, Myanmar). This unique blend of qualifications
+      provides me with a refined sense of design aesthetics and a deep
+      understanding of technology. By seamlessly integrating creativity with
+      technical expertise, I offer a distinctive perspective that bridges the
+      gap between form and function, delivering exceptional solutions in the
+      field of front-end development.
+    </p>
+    <div class="flex items-center mt-10 mb-16 gap-6 flex-wrap">
+      <p class="text-lg italic">Tech stacks:</p>
+      <div class="flex gap-6">
+        <img class="h-6" src="/assets/html_logo.png" alt="" />
+        <img class="h-6" src="/assets/css_logo.png" alt="" />
+        <img class="h-6" src="/assets/scss_logo.png" alt="" />
+        <img class="h-6" src="/assets/tailwind_logo.png" alt="" />
+        <img class="h-6" src="/assets/bootstrap_logo.png" alt="" />
+        <img class="h-6" src="/assets/javascript_logo.png" alt="" />
+        <img class="h-6" src="/assets/typescript_logo.png" alt="" />
+        <img class="h-6" src="/assets/vue_logo.png" alt="" />
+        <img class="h-6" src="/assets/vite_logo.png" alt="" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
