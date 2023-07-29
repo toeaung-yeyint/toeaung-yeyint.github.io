fbc692b89c8f5f60fc52e77710b008e82dfb9f60
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2023-07-30 01:14:55 +1000

    added tech stacks section

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,35 +1,35 @@
 <template>
-  <div class="flex flex-col gap-x-8 mt-12 mb-16 md:mt-16 md:mb-24">
-    <h1 class="text-center text-4xl font-bold mb-10">Toe Aung Ye Yint</h1>
-    <p class="text-base">
-      I am a skilled front-end developer with a diverse educational background,
-      I hold a Master's degree in Information Technology (Monash University,
-      Australia) alongside a Bachelor's degree in Architecture (Thanlyin
-      Technological University, Myanmar). This unique blend of qualifications
-      provides me with a refined sense of design aesthetics and a deep
-      understanding of technology. By seamlessly integrating creativity with
-      technical expertise, I offer a distinctive perspective that bridges the
-      gap between form and function, delivering exceptional solutions in the
-      field of front-end development.
-    </p>
-    <div class="flex items-center mt-10 gap-6 flex-wrap">
-      <p class="text-xl italic font-medium">My tech stacks:</p>
-      <div class="flex gap-6 justify-center flex-wrap">
-        <img class="h-8" src="/assets/html_logo.png" alt="" />
-        <img class="h-8" src="/assets/css_logo.png" alt="" />
-        <img class="h-8" src="/assets/scss_logo.png" alt="" />
-        <img class="h-8" src="/assets/tailwind_logo.png" alt="" />
-        <img class="h-8" src="/assets/bootstrap_logo.png" alt="" />
-        <img class="h-8" src="/assets/javascript_logo.png" alt="" />
-        <img class="h-8" src="/assets/typescript_logo.png" alt="" />
-        <img class="h-8" src="/assets/vue_logo.png" alt="" />
-        <img class="h-8" src="/assets/vite_logo.png" alt="" />
-        <img class="h-8" src="/assets/sql_logo.png" alt="" />
-        <img class="h-8" src="/assets/mongodb_logo.png" alt="" />
-        <img class="h-8" src="/assets/firebase_logo.png" alt="" />
-      </div>
-    </div>
-  </div>
+	<div class="flex flex-col gap-x-8 mt-12 mb-16 md:mt-16 md:mb-24">
+		<h1 class="text-center text-4xl font-bold mb-10">Toe Aung Ye Yint</h1>
+		<p class="text-base">
+			I am a skilled front-end developer with a diverse educational background,
+			I hold a Master's degree in Information Technology (Monash University,
+			Australia) alongside a Bachelor's degree in Architecture (Thanlyin
+			Technological University, Myanmar). This unique blend of qualifications
+			provides me with a refined sense of design aesthetics and a deep
+			understanding of technology. By seamlessly integrating creativity with
+			technical expertise, I offer a distinctive perspective that bridges the
+			gap between form and function, delivering exceptional solutions in the
+			field of front-end development.
+		</p>
+		<div class="flex items-center mt-10 gap-6 flex-wrap">
+			<p class="text-xl italic font-medium">My tech stacks:</p>
+			<div class="flex gap-6 justify-center flex-wrap">
+				<img class="h-8" src="/assets/html_logo.png" alt="" />
+				<img class="h-8" src="/assets/css_logo.png" alt="" />
+				<img class="h-8" src="/assets/scss_logo.png" alt="" />
+				<img class="h-8" src="/assets/tailwind_logo.png" alt="" />
+				<img class="h-8" src="/assets/javascript_logo.png" alt="" />
+				<img class="h-8" src="/assets/typescript_logo.png" alt="" />
+				<img class="h-8" src="/assets/react_logo.png" alt="" />
+				<img class="h-8" src="/assets/vue_logo.png" alt="" />
+				<img class="h-8" src="/assets/vite_logo.png" alt="" />
+				<img class="h-8" src="/assets/sql_logo.png" alt="" />
+				<img class="h-8" src="/assets/mongodb_logo.png" alt="" />
+				<img class="h-8" src="/assets/firebase_logo.png" alt="" />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup></script>
